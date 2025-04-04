01c01bd86fa06559e46285763d5152e3cb4c38f9
Lorant Marton <Lorant.Marton@BOLYAI.LOCAL>
2025-04-04 11:43:37 +0200

    And take my hands, they'll understand

--- 1_promise_async/main.js
+++ 1_promise_async/main.js
@@ -1,8 +1,10 @@
+
+//----------------------------------------------------------------------Promise és a 2 statisuk függvénye és a .then() és .catch
 const buyApple1 = (apple1 => {
     if(apple1 < 5){
-        return Promise.resolve('Van elég alma!');//Felóldott promise
+        return Promise.resolve('Van elég alma+!');//Felóldott promise
     }else{
-        return Promise.reject('Nincs elég alma!');
+        return Promise.reject('Nincs elég alma+!');
     }
 })
 
@@ -15,6 +17,57 @@ res1.then(value => {
 console.log('Feloldás után')
 
 buyApple1(5).then(value => {
+    console.log(value);
+}).catch(error => {
+    console.log(error);
+});
+
+
+const buyApple2 = (apple2 => {
+    return new Promise((resolve, reject)=> {//böngészőbe van impletentálva (resolve, reject)
+        if(apple2 < 5){
+            resolve('van alma++:'+apple2);
+        }else{
+            reject('Nincs elég alma++:'+apple2);
+        }
+    });
+});
+
+buyApple2(3).then(value => {
+    console.log(value);
+}).catch(error => {
+    console.log(error);
+});
+
+buyApple2(5).then(value => {
+    console.log(value);
+}).catch(error => {
+    console.log(error);
+});
+
+//----------------------------------------------------------timeout
+
+const buyApple3 = (apple3 => {
+    return new Promise((resolve, reject)=> {//böngészőbe van impletentálva (resolve, reject)
+        if(apple3 < 5){
+            setTimeout(() => {
+                resolve('van alma+++:'+apple3);
+            }, 2000);
+        }else{
+            setTimeout(() => {
+                reject('Nincs alma+++:'+apple3);
+            }, 3000);
+        }
+    });
+});
+
+buyApple3(3).then(value => {
+    console.log(value);
+}).catch(error => {
+    console.log(error);
+});
+
+buyApple3(5).then(value => {
     console.log(value);
 }).catch(error => {
     console.log(error);
